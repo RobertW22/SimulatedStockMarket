e957bc172f5a55db46ed4d0ebf318eea5d33f056
Robert <robertweeden2@outlook.com>
2023-01-04 12:52:57 +1100

    Initial commit
    First commit

---
+++ MainStockFloor.java
@@ -0,0 +1,17 @@
+// create a main function
+
+
+public class MainStockFloor {
+    // create a method
+
+    public static void main(String[] args) {
+        // create a new instance of the class
+        // and call the method
+        System.out.print("hi");
+    }
+
+
+
+}
+
+
